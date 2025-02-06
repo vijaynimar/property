b9b2b3c63ef4a09ecd25e7596169561e0203b63a
vijaynimar <vijaynimar8@gmail.com>
2025-02-06 11:53:09 +0530

    mongo

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import "dotenv/config"
 
 const connection=async()=>{
-    const url=process.env.Mongo_url
+    // const url=process.env.Mongo_url
     try{
-       await mongoose.connect(url)
+       await mongoose.connect("mongodb+srv://vijaynimar:X2DCUTXzFRG9L2ec@property.ylzok.mongodb.net/?retryWrites=true&w=majority&appName=property")
         console.log("connected to mongoDb");
     }catch(err){
         console.log(err);
