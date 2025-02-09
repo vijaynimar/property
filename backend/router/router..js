c4c9000670decb4e3cef7704b1decf5a5f70b408
vijaynimar <vijaynimar8@gmail.com>
2025-02-09 13:49:06 +0530

    profile

--- backend/router/router..js
+++ backend/router/router..js
@@ -12,5 +12,5 @@ router.post("/log-in",login)
 router.post("/otp-register",sendRegisterOtp)
 router.post("/uploadProperty",upload,sell)
 router.get("/propertyget",getProperty)
-router.post("/profileUpload",profileMulter("profile"),uploadProfile)
+router.post("/profileUpload",profileMulter.single("profile"),uploadProfile)
 export {router}
